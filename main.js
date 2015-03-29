53f330af6c3ed14d76e41bfce68c2ebea5c24a1b
PascalCremer <pascal.cremer@gmx.de>
2015-03-29 12:52:40 +0200

    Clean up

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-var update_position = function(sphere, all_spheres) {
+var camera, scene, renderer;
+
+init()
+render()
+
+function update_position(sphere, all_spheres) {
 	sphere.position.x += 0.01
 }
 
-var scene = new THREE.Scene();
-var camera = new THREE.OrthographicCamera( -10, 10, -10, 10, -1, 1);
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.OrthographicCamera( -10, 10, -10, 10, -1, 1);
+	
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	var sphere;
+	var radius = 0.1;
+	var num_particles = 10;
 
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var sphere;
-var radius = 0.1;
-var num_particles = 10;
-
-for (var i = 0; i < num_particles; i++) {
-	sphere = new THREE.Mesh( new THREE.SphereGeometry( radius ), material );
-	sphere.position.set( -2*i + num_particles, 0, 0 );
-	scene.add(sphere)
+	for (var i = 0; i < num_particles; i++) {
+		sphere = new THREE.Mesh( new THREE.SphereGeometry( radius ), material );
+		sphere.position.set( -2*i + num_particles, 0, 0 );
+		scene.add(sphere)
+	}
 }
 
-var render = function () {
+
+function render() {
 	requestAnimationFrame( render );
 
 	for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
@@ -32,5 +40,3 @@ var render = function () {
 
 	renderer.render(scene, camera);
 };
-
-render();
