82c9f1c81ec99bc94a787c4c403c21bb17dffce6
PascalCremer <pascal.cremer@gmx.de>
2015-03-29 18:10:15 +0200

    ...

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 var camera, scene, renderer;
 
-init()
-render()
+var A=0.01;
+var B=0.01;
 
-function update_position(sphere, all_spheres) {
-	sphere.position.x += 0.01
+var velocities = [];
+
+init();
+render();
+
+function lennard_jones_force(from_idx, to_idx) {
+	var force = new THREE.Vector3(0,0,0);
+	force.subVectors(scene.children[to_idx].position, scene.children[from_idx].position);
+	force.normalize();
+
+	var d = scene.children[from_idx].position.distanceTo( scene.children[to_idx].position );
+	var strength = -6*A*Math.pow(d, -7) + 12*B*Math.pow(d, -13);
+
+	force.multiplyScalar(strength);
+	return force;
+}
+
+function update_position(sphere_idx) {
+	var force = new THREE.Vector3(0,0,0);
+	for (var i = 0; i < scene.children.length; i++) {
+		if (i == sphere_idx) { continue; }
+		force.add(lennard_jones_force(i, sphere_idx));
+	}
+	velocities[sphere_idx].add(force);
+	scene.children[sphere_idx].position.add(velocities[sphere_idx]);
 }
 
 function init() {
@@ -23,6 +46,7 @@ function init() {
 	for (var i = 0; i < num_particles; i++) {
 		sphere = new THREE.Mesh( new THREE.SphereGeometry( radius ), material );
 		sphere.position.set( -2*i + num_particles, 0, 0 );
+		velocities.push(new THREE.Vector2(0,0,0))
 		scene.add(sphere)
 	}
 }
@@ -34,8 +58,7 @@ function render() {
 	for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
 
 		var sphere = scene.children[ i ];
-		update_position(sphere, [])
-
+		//update_position(i);
 	}
 
 	renderer.render(scene, camera);
